c5333bb68f1ce5c21eac4850d9a8ffe38cd1d04a
Daniel Matthew Prince <programmer96@hotmail.com>
2015-05-06 21:38:23 +0000

    Added plan model and database table

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,12 @@
-class ContactMailer < ActionMailer::Base
-  default to: "programmer96@hotmail.com"
+#class ContactMailer < ActionMailer::Base
+#  default to: "programmer96@hotmail.com"
   
   
-  def contact_email(name, email, body)
-    @name = name
-    @email = email
-    @body = body
+#  def contact_email(name, email, body)
+#    @name = name
+ #   @email = email
+  #  @body = body
     
-    mail(from: email, subject: "Contact Form Message")
-  end
-end
+   # mail(from: email, subject: "Contact Form Message")
+  #end
+#end
